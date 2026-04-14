b0f8d30d47b948379d4c9f93363b7b0af0b66f2a
Jiyaas05 <jsawant01@qub.ac.uk>
2026-04-14 01:36:11 +0100

    why no uploading wtf

--- BooleanSimulator-Jiyaa.cpp
+++ BooleanSimulator-Jiyaa.cpp
@@ -2,3 +2,9 @@
 #include <iostream>
 #include <string>
 using namespace std;
+//trying to get this to upload to git hub
+
+int main() {
+    cout << "Boolean Simulator starting..." << endl;
+    return 0;
+}
