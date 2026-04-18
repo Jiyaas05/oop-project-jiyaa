8664205e2dceeb576cd13ac318b06e6026d1cbc1
Jiyaas05 <jsawant01@qub.ac.uk>
2026-04-18 20:54:04 +0100

    added right display format

--- BooleanSimulator-Jiyaa.cpp
+++ BooleanSimulator-Jiyaa.cpp
@@ -1,11 +1,12 @@
 // Jiyaa Satish Sawant, 40471898
+#include <fstream>
 #include <iostream>
 #include <string>
-#include <vector> 
+#include <vector>
 using namespace std;
 
 // Stores the expression the user enters
-class BooleanExpression { 
+class BooleanExpression {
 public:
     string text;
 
@@ -43,6 +44,7 @@ string toUpperCase(string s) {
     }
     return result;
 }
+
 string removeBrackets(string s) {
     string out = "";
     for (char c : s) {
@@ -62,13 +64,12 @@ bool evalToken(string t, bool A, bool B, bool C) {
     return false;
 }
 
-// Evaluates Boolean expressions 
 bool evaluateExpression(string expr, bool A, bool B, bool C) {
     vector<string> p = split(expr);
 
     for (int i = 0; i < p.size(); i++) {
         if (p[i] == "NOT" && i + 1 < p.size()) {
-            bool val = evalToken(p[i+1], A, B, C);
+            bool val = evalToken(p[i + 1], A, B, C);
             p[i] = val ? "FALSE" : "TRUE";
             p.erase(p.begin() + i + 1);
         }
@@ -81,9 +82,9 @@ bool evaluateExpression(string expr, bool A, bool B, bool C) {
 
     bool result = evalToken(p[0], A, B, C);
 
-    for (int i = 1; i < p.size(); i += 2) {
+    for (int i = 1; i + 1 < p.size(); i += 2) {
         string op = p[i];
-        bool right = evalToken(p[i+1], A, B, C);
+        bool right = evalToken(p[i + 1], A, B, C);
 
         if (op == "AND") result = result && right;
         else if (op == "OR") result = result || right;
@@ -95,7 +96,6 @@ bool evaluateExpression(string expr, bool A, bool B, bool C) {
     return result;
 }
 
-
 class TruthTable {
 public:
     string expr;
@@ -103,39 +103,56 @@ public:
     TruthTable(string e) {
         expr = e;
     }
+
     void printTable() {
-        cout << "A B C | Result\n";
-        cout << "----------------\n";
+        cout << "| A | B | C | RESULT |\n";
+        cout << "|---|---|---|--------|\n";
+
+        string clean = removeBrackets(expr);
 
         for (int a = 0; a < 2; a++) {
             for (int b = 0; b < 2; b++) {
                 for (int c = 0; c < 2; c++) {
-                    bool r;
-
-                    string clean = removeBrackets(expr);
-
-                    int spaces = 0;
-                    for (char ch : clean) {
-                        if (ch == ' ') {
-                            spaces++;
-                        }
-                    }
-
-                    r = evaluateExpression(clean, a, b, c);
-
-                    cout << a << " " << b << " " << c << " | " << r << endl;
+                    bool r = evaluateExpression(clean, a, b, c);
+                    cout << "| " << a << " | " << b << " | " << c << " |   " << r << "    |\n";
                 }
             }
+        }
+
+        cout << "\nWould you like to save this expression and truth table to a file? (Y/N): ";
+        char saveChoice;
+        cin >> saveChoice;
+
+        if (saveChoice == 'Y' || saveChoice == 'y') {
+            cout << "Enter filename: ";
+            string filename;
+            cin >> filename;
+
+            ofstream file(filename);
+            file << "Truth table for: " << expr << "\n\n";
+            file << "| A | B | C | RESULT |\n";
+            file << "|---|---|---|--------|\n";
+
+            for (int a = 0; a < 2; a++) {
+                for (int b = 0; b < 2; b++) {
+                    for (int c = 0; c < 2; c++) {
+                        bool r = evaluateExpression(clean, a, b, c);
+                        file << "| " << a << " | " << b << " | " << c << " |   " << r << "    |\n";
+                    }
+                }
+            }
+
+            file.close();
+            cout << "\nExpression and truth table saved successfully!\n";
         }
     }
 };
 
-int main()
-{
+int main() {
     bool running = true;
-    // Main loop 
+
     while (running) {
-        cout << "BOOLEAN TRUTH TABLE\n";
+        cout << "BOOLEAN TRUTH TABLE SIMULATION\n";
         cout << "1. Enter expression\n";
         cout << "2. Exit\n";
         cout << "Choice: ";
@@ -143,23 +160,26 @@ int main()
         int choice;
         cin >> choice;
         cin.ignore();
+
         if (choice == 1) {
             cout << "Enter expression (In capital letters): ";
             string expr;
             getline(cin, expr);
-            // Convert to uppercase
+            cout << "\nOperators Detected and Explained:\n";
+            cout << "- AND: True only if both inputs are true.\n";
+            cout << "- OR: True if at least one input is true.\n";
+            cout << "- NOT: Inverts the input.\n\n";
+
             expr = toUpperCase(expr);
-            // prints the tablr
             BooleanExpression bexpr(expr);
             TruthTable table(expr);
             table.printTable();
-        }
-        else if (choice == 2) {
+        } else if (choice == 2) {
             running = false;
-        }
-        else {
+        } else {
             cout << "Invalid choice." << endl;
         }
     }
+
     return 0;
 }
